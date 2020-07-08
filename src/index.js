19a4eff8444ffd10a494b468629cee8ccd8ba318
hauru <iker21@rambler.ru>
2020-07-08 13:15:30 +0300

    Добавил лоадеры

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import Post from './Post';
+import './styles/styles.css';
+import json from '../assets/json.json';
+import WebpackLogo from '../assets/webpack-logo.png';
+import xml from '../assets/data.xml';
+import csv from '../assets/data.csv';
 
-const post = new Post('Webpack Post Title');
+const post = new Post('Webpack Post Title', WebpackLogo);
 
 console.log('Post', post.toString());
+
+console.log('JSON', json);
+
+console.log('XML', xml);
+
+console.log('CSV', csv);
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './src/index.js',
-        analytics: './src/analytics.js'
+        main: './index.js',
+        analytics: './analytics.js'
     },
     output: {
         filename: '[name].[contenthash].js',
@@ -15,8 +16,32 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './src/index.html'
+            template: './index.html'
         }),
         new CleanWebpackPlugin()
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader']
+            },
+        ]
+    }
 }
